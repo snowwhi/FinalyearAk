201aa64950e9d4bd563aa28b1cb07b2465779fa5
Sarfraz Afzal <sarfrazlqp03@gmail.com>
2026-05-31 05:37:44 +0500

    done

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://finalyear-ak.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
